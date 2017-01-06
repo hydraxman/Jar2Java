71185d89b3d404f7f49351ec23ca1679ed08a4b4
Bu,Shaopeng <bushaopeng@baidu.com>
2017-01-06 17:20:39 +0800

    init proj first functional version

---
+++ src/main/java/com/bryansharp/jar2java/Decompiler.java
@@ -0,0 +1,35 @@
+package com.bryansharp.jar2java;
+
+import jd.commonide.preferences.IdePreferences;
+
+/**
+ * Created by bushaopeng on 17/1/6.
+ */
+public class Decompiler {
+    /**
+     * Actual call to the native lib.
+     *
+     * @param basePath          Path to the root of the classpath, either a path to a directory or a path to a jar file.
+     * @param internalTypeName  internal name of the type.
+     * @return Decompiled class text.
+     */
+    public static String decompile(String basePath, String internalTypeName) {
+        // Load preferences
+
+        boolean showDefaultConstructor = true;
+        boolean realignmentLineNumber = true;
+        boolean showPrefixThis = false;
+        boolean mergeEmptyLines = false;
+        boolean unicodeEscape = false;
+        boolean showLineNumbers = false;
+        boolean showMetadata = false;
+
+        // Create preferences
+        IdePreferences preferences = new IdePreferences(
+                showDefaultConstructor, realignmentLineNumber, showPrefixThis,
+                mergeEmptyLines, unicodeEscape, showLineNumbers, showMetadata);
+
+        // Decompile
+        return IdeDecompiler.decompile(preferences, basePath, internalTypeName);
+    }
+}
