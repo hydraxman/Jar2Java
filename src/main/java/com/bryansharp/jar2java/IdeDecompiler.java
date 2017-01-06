aab4218d53b8545727500cb0687a4e701fafff32
Bu,Shaopeng <bushaopeng@baidu.com>
2017-01-06 17:20:39 +0800

    init proj first functional version

---
+++ src/main/java/com/bryansharp/jar2java/IdeDecompiler.java
@@ -0,0 +1,77 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+package com.bryansharp.jar2java;
+
+import jd.common.loader.BaseLoader;
+import jd.common.loader.LoaderManager;
+import jd.common.printer.text.PlainTextPrinter;
+import jd.common.util.CommonTypeNameUtil;
+import jd.common.util.VersionUtil;
+import jd.commonide.preferences.IdePreferences;
+import jd.core.loader.LoaderException;
+import jd.core.model.classfile.ClassFile;
+import jd.core.model.reference.ReferenceMap;
+import jd.core.process.analyzer.classfile.ClassFileAnalyzer;
+import jd.core.process.analyzer.classfile.ReferenceAnalyzer;
+import jd.core.process.deserializer.ClassFileDeserializer;
+import jd.core.process.layouter.ClassFileLayouter;
+import jd.core.process.writer.ClassFileWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class IdeDecompiler {
+    private static LoaderManager loaderManager = new LoaderManager();
+
+    public IdeDecompiler() {
+    }
+
+    public static String decompile(IdePreferences preferences, String basePath, String classPath) {
+        try {
+            BaseLoader t = loaderManager.getLoader(basePath);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(10240);
+            PrintStream ps = new PrintStream(baos);
+            PlainTextPrinter printer = new PlainTextPrinter(preferences, ps);
+            ClassFile classFile = ClassFileDeserializer.Deserialize(t, classPath);
+            if(classFile == null) {
+                throw new LoaderException("Can not deserialize \'" + classPath + "\'.");
+            } else {
+                ReferenceMap referenceMap = new ReferenceMap();
+                ClassFileAnalyzer.Analyze(referenceMap, classFile);
+                ReferenceAnalyzer.Analyze(referenceMap, classFile);
+                ArrayList layoutBlockList = new ArrayList(1024);
+                int maxLineNumber = ClassFileLayouter.Layout(preferences, referenceMap, classFile, layoutBlockList);
+                ClassFileWriter.Write(t, printer, referenceMap, maxLineNumber, classFile.getMajorVersion(), classFile.getMinorVersion(), layoutBlockList);
+                if(preferences.isShowMetadata()) {
+                    printer.endOfLine();
+                    printer.print("/* Location:           ");
+                    printer.print(t.getCodebase());
+                    printer.endOfLine();
+                    printer.print(" * Qualified Name:     ");
+                    String qualifiedName = CommonTypeNameUtil.InternalPathToQualifiedTypeName(classPath);
+                    printer.print(qualifiedName);
+                    String jdkVersion = VersionUtil.getJDKVersion(classFile.getMajorVersion(), classFile.getMinorVersion());
+                    if(jdkVersion.length() > 0) {
+                        printer.endOfLine();
+                        printer.print(" * Java Class Version: ");
+                        printer.print(jdkVersion);
+                    }
+
+                    printer.endOfLine();
+                    printer.print(" * JD-Core Version:    ");
+                    printer.print("0.7.1");
+                    printer.endOfLine();
+                    printer.print(" */");
+                }
+
+                ps.close();
+                return new String(baos.toByteArray());
+            }
+        } catch (Throwable var13) {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/bryansharp/jar2java/Main.java
@@ -0,0 +1,97 @@
+package com.bryansharp.jar2java;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * Created by bushaopeng on 17/1/6.
+ */
+public class Main {
+    private static File baseDir;
+
+    public static void main(String[] args) {
+        if (args == null || args.length < 1) {
+            log("please specify a jar file");
+            return;
+        }
+        try {
+            ArrayList<String> paths = new ArrayList<>();
+            for (String jarFile : args) {
+                if (checkJar(jarFile)) {
+                    paths.add(jarFile);
+                }
+            }
+            if(paths.size()>0){
+                initBuildPath();
+                for (String jarFile : paths) {
+                    printJar(jarFile);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initBuildPath() {
+        String dirName = getOutputFileDirName();
+        baseDir = new File(dirName);
+        baseDir.mkdirs();
+    }
+
+    private static boolean checkJar(String jarPath) {
+        return !(jarPath == null || !jarPath.endsWith(".jar") || !new File(jarPath).exists());
+    }
+
+    private static void printJar(String jarFullPath) throws IOException {
+        ZipFile zipFile = new ZipFile(jarFullPath);
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry zipEntry = entries.nextElement();
+            String name = zipEntry.getName();
+            if (name.endsWith(".class")) {
+                log("start to show the code of " + name + "\n=======\ndisplayed as below:\n=======\n");
+                String result = Decompiler.decompile(jarFullPath, name);
+                log(result);
+                stringToClassFile(name, result);
+            }
+        }
+    }
+
+    private static void stringToClassFile(String name, String result) throws IOException {
+        int endIndex = name.lastIndexOf("/");
+        String dirName = baseDir.getAbsolutePath() + "/" + name.substring(0, endIndex);
+        File dir = new File(dirName);
+        dir.mkdirs();
+        String javaFilename = name.substring(endIndex + 1).replace(".class", ".java");
+        File javaFile = new File(dir, javaFilename);
+        FileOutputStream fileOutputStream = new FileOutputStream(javaFile);
+        fileOutputStream.write(result.getBytes());
+        fileOutputStream.flush();
+        javaFile.setWritable(true, false);
+    }
+
+    final static String pathname = "build/javaDirOutput";
+
+    private static String getOutputFileDirName() {
+        if (new File(pathname).exists()) {
+            return getNewDirName(1);
+        }
+        return pathname;
+    }
+
+    private static String getNewDirName(int count) {
+        if (new File(pathname + count).exists()) {
+            return getNewDirName(++count);
+        }
+        return pathname + count;
+    }
+
+    public static void log(Object msg) {
+        System.out.println(msg.toString());
+    }
+}
